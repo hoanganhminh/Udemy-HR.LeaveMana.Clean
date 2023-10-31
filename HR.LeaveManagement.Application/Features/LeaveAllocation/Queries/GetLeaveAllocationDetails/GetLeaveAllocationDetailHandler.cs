2fbddb83e1a776078f6e93c18dac09ad06eab76d
hoanganhminh <hoanganhminh120601@gmail.com>
2023-10-31 11:22:47 +0700

    45. Complete Leave Allocation Feature

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Queries/GetLeaveAllocationDetails/GetLeaveAllocationDetailHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails
+{
+    public class GetLeaveAllocationDetailHandler : IRequestHandler<GetLeaveAllocationDetailRequestQuery,LeaveAllocationDetailDto>
+    {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly Mapper _mapper;
+
+        public GetLeaveAllocationDetailHandler(ILeaveAllocationRepository leaveAllocationRepository, Mapper mapper)
+        {
+            this._leaveAllocationRepository = leaveAllocationRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<LeaveAllocationDetailDto> Handle(GetLeaveAllocationDetailRequestQuery request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.GetLeaveAllocationWithDetails(request.Id);
+            return _mapper.Map<LeaveAllocationDetailDto>(leaveAllocation);
+        }
+    }
+}
--- HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
+++ HR.LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -18,7 +18,7 @@ namespace HR.LeaveManagement.Persistence.Repositories
 
         public async Task<bool> IsLeaveTypeUnique(string name)
         {
-            return await _context.LeaveTypes.AnyAsync(t => t.Name == name);
+            return await _context.LeaveTypes.AnyAsync(t => t.Name == name) == false;
         }
     }
 }
