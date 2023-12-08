4c180cdd5df7e659c22a527006007e7389a8ff07
hoanganhminh <hoanganhminh120601@gmail.com>
2023-12-08 17:48:24 +0700

    done Section 7: Unit and Integration Testing

--- HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -4,15 +4,10 @@ using HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
 using HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
 using HR.LeaveManagement.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.MappingProfiles
 {
-    internal class LeaveTypeProfile : Profile
+    public class LeaveTypeProfile : Profile
     {
         public LeaveTypeProfile()
         {
